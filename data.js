8cc185ec67fda2ee175bb53646d010b6fb216b59
Ryan Peacock <Ryan@Ryans-MacBook-Pro.local>
2023-08-03 20:32:43 -0700

    added new catechism data file and made new route to serve that data. also installed express and nodemon

---
+++ data.js
@@ -0,0 +1,131 @@
+const largerCatechism = [
+  {
+    id: 1,
+    question: "What is the chief and highest end of man?",
+    answer: [
+      "Man’s chief and highest end is to glorify God,",
+      "and fully to enjoy him forever.",
+    ],
+    proofTexts: [
+      [
+        {
+          book: "Romans",
+          verse: "11:36",
+          text: "",
+        },
+        {
+          book: "1 Corinthians",
+          verse: "10:31",
+          text: "",
+        },
+      ],
+      [
+        {
+          book: "Psalms",
+          verse: "73:24-28",
+          text: "",
+        },
+      ],
+    ],
+  },
+  {
+    id: 2,
+    question: "How doth it appear that there is a God?",
+    answer: [
+      "The very light of nature in man, and the works of God, declare plainly that there is a God;",
+      "but his word and Spirit only do sufficiently and effectually reveal him unto men for their salvation.",
+    ],
+    proofTexts: [
+      [
+        {
+          book: "Romans",
+          verse: "1:19-20",
+          text: "",
+        },
+        {
+          book: "Psalms",
+          verse: "19:1-3",
+          text: "",
+        },
+        {
+          book: "Acts",
+          verse: "17:28",
+          text: "",
+        },
+      ],
+      [
+        {
+          book: "1 Corinthians",
+          verse: "2:9-10",
+          text: "",
+        },
+        {
+          book: "2 Timothy",
+          verse: "3:15-17",
+          text: "",
+        },
+        {
+          book: "Isaiah",
+          verse: "59:21",
+          text: "",
+        },
+      ],
+    ],
+  },
+  {
+    id: 3,
+    question: "What is the word of God?",
+    answer: [
+      "The holy scriptures of the Old and New Testaments are the word of God,",
+      "the only rule of faith and obedience.",
+    ],
+    proofTexts: [
+      [
+        {
+          book: "2 Timothy",
+          verse: "3:16",
+          text: "",
+        },
+        {
+          book: "2 Peter",
+          verse: "1:19-21",
+          text: "",
+        },
+      ],
+      [
+        {
+          book: "Ephesians",
+          verse: "2:20",
+          text: "",
+        },
+        {
+          book: "Revelation",
+          verse: "22:18-19",
+          text: "",
+        },
+        {
+          book: "Isaiah",
+          verse: "8:20",
+          text: "",
+        },
+        {
+          book: "Luke",
+          verse: "16:29, 31",
+          text: "",
+        },
+        {
+          book: "Galatians",
+          verse: "1:8-9",
+          text: "",
+        },
+        {
+          book: "2 Timothy",
+          verse: "3:15-16",
+          text: "",
+        },
+      ],
+    ],
+  },
+];
+
+module.exports = { largerCatechism };
